d6e620a8730b6d7376420ac9a84e49f59e67d6ce
Adunni <otungprincess@gmail.com>
2023-01-04 16:07:58 +0000

    render a table and for each elements in the array

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,40 @@
+/** table based on a list of objects */
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const studentA: Student = {
+  firstName: 'Favour',
+  lastName: 'Chijioke',
+  age: 29,
+  location: 'Lagos',
+}
+
+const studentB: Student = {
+  firstName: 'Bounty',
+  lastName: 'Moon',
+  age: 18,
+  location: 'Connecticut',
+}
+
+const studentList = [studentA, studentB];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentList.forEach((obj) => {
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
+  const cellLocation = document.createElement('td');
+  cellName.textContent = obj.firstName;
+  cellLocation.textContent = obj.location;
+  row.appendChild(cellName);
+  row.appendChild(cellLocation);
+  tbody.appendChild(row);
+});
+table.appendChild(tbody);
+document.body.appendChild(table)
